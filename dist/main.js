e6234583ffbcf79c1352947e7393c9d4f9f0733f
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-12-21 10:51:25 +0100

    Create responsive header

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,12 @@
-/** *** */ (() => {
-  // webpackBootstrap
-  const __webpack_exports__ = {};
-  /*! **********************!*\
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
   !*** ./src/index.js ***!
-  \********************* */
-
-  /** *** */
-})();
+  \**********************/
+const navbarButton = document.querySelector('.line-container');
+navbarButton.addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('active')
+})
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL3BvcnRmb2xpby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBuYXZiYXJCdXR0b24gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcubGluZS1jb250YWluZXInKTtcbm5hdmJhckJ1dHRvbi5hZGRFdmVudExpc3RlbmVyKCdjbGljaycsICgpID0+IHtcbiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcubmF2YmFyJykuY2xhc3NMaXN0LnRvZ2dsZSgnYWN0aXZlJylcbn0pIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const navbarButton = document.querySelector('.line-container');
+navbarButton.addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('active')
+})
