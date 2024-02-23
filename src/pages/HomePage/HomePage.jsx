669446b6e4ec4e6a3595eae1166c0a3a2f04922b
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-23 21:34:38 +0100

    Create landing section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,15 @@
+import './HomePage.scss';
+
 export const HomePage = () => {
-  return <div>This is home page</div>;
+  return (
+    <div className="page home-page">
+      <div className="landing-section">
+        <div>PHOTO</div>
+        <h5 className="text-primary-color">Hey, I am</h5>
+        <h1 className="landing-heading">Stjepan Franić</h1>
+        <p className="text">A junior front-end developer from Croatia.</p>
+        <button className="button button-primary">More About Me</button>
+      </div>
+    </div>
+  );
 };
