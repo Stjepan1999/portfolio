0865cf7b6ee26ac5e16fcf29119897b890066c7a
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-24 19:26:28 +0100

    Add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Sidebar } from '../Sidebar/Sidebar';
 export const Header = () => {
   return (
     <header className="header">
-      <NavLink to="/" className="nav-link">
+      <NavLink to="/" className="nav-link logo">
         Stjepan
       </NavLink>
       <nav className="nav-bar">
