8be22270627a16335002785da69ddc7cd4d7a0c4
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-26 19:11:44 +0100

    Edit landing section and add image

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,20 @@
 import './HomePage.scss';
+import landingImage from '../../assets/Images/landing-image.png';
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
   return (
     <div className="page home-page">
       <div className="landing-section">
-        <div>PHOTO</div>
-        <h5 className="text-primary-color">Hey, I am</h5>
-        <h1 className="landing-heading">Stjepan Franić</h1>
+        <div>
+          <img src={landingImage} className="landing-image" />
+        </div>
+        <h5 className="text-primary-color medium-text">Hey, I'm</h5>
+        <h1 className="large-text">Stjepan Franić</h1>
         <p className="text">A junior front-end developer from Croatia.</p>
-        <button className="button button-primary">More About Me</button>
+        <Link to="/about" className="button button-primary margin-top">
+          More About Me
+        </Link>
       </div>
     </div>
   );
