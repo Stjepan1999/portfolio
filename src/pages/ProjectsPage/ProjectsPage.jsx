6cb5f3bfff87d3d0cb59352ac48508dd5d666e7d
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-02-26 19:01:27 +0100

    Add projects

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,5 +1,8 @@
 import './ProjectsPage.scss';
-import ecomImage from '../../assets/Images/ecom-project.png';
+import ecomImage from '../../assets/Images/project-ecom.png';
+import cvbuilderImage from '../../assets/Images/project-cvbuilder.png';
+import weatherImage from '../../assets/Images/project-weather.png';
+import todoImage from '../../assets/Images/project-todo.png';
 
 export const ProjectsPage = () => {
   return (
@@ -8,14 +11,47 @@ export const ProjectsPage = () => {
         <h1 className="text text-heading">
           My <span className="text-primary-color">Projects</span>
         </h1>
-        <Project
-          title="Ecommerce Website"
-          stack="React.js, SCSS, React Router"
-          description="A React web application that simulates an ecommerce website. It utilizes the Fake Store API to fetch product data and allows users to browse products, add items to their shopping cart, and view information about the store."
-          img={ecomImage}
-          liveLink="https://ecomwebsite-stjepan.netlify.app/"
-          sourceLink="https://github.com/Stjepan1999/ecommerce-website"
-        />
+        <div className="projects-container">
+          <Project
+            title="Ecommerce Website"
+            stack="React.js, SCSS, React Router"
+            description="A React web application that simulates an ecommerce website. It utilizes the Fake Store API to fetch product data and allows users to browse products, add items to their shopping cart, and view information about the store."
+            img={ecomImage}
+            liveLink="https://ecomwebsite-stjepan.netlify.app/"
+            sourceLink="https://github.com/Stjepan1999/cv-builder"
+          />
+          <Project
+            title="CV Builder"
+            stack="React.js"
+            description="A React web application for creating resumes. Users can download their CV in a formatted PDF file. Users can add and edit sections such as personal info, contact info, education and experience section, and skills."
+            img={cvbuilderImage}
+            liveLink="https://jade-gumdrop-ae72b5.netlify.app/"
+            sourceLink="https://github.com/Stjepan1999/ecommerce-website"
+          />
+          <Project
+            title="Weather App"
+            stack="HTML, CSS, JavaScript"
+            description="A simple weather app built with JavaScript, HTML, CSS. It uses Visual Crossing API to get weather information, and provides current weather conditions, and 5 days forecast."
+            img={weatherImage}
+            liveLink="https://stjepan1999.github.io/weather-app/"
+            sourceLink="https://github.com/Stjepan1999/weather-app"
+          />
+          <Project
+            title="To-Do List"
+            stack="HTML, CSS, JavaScript"
+            description="To-Do List web application where users can organize tasks by creating custom projects. Data is saved to local storage."
+            img={todoImage}
+            liveLink="https://stjepan1999.github.io/todo-list/"
+            sourceLink="https://github.com/Stjepan1999/todo-list"
+          />
+        </div>
+        <a
+          className="button  button-primary-transparent margin-bottom"
+          href="https://github.com/Stjepan1999?tab=repositories"
+          target="_blank"
+        >
+          See All
+        </a>
       </div>
     </div>
   );
@@ -40,7 +76,7 @@ const Project = ({ title, stack, description, img, liveLink, sourceLink }) => {
           </a>
         </div>
       </div>
-      <div>
+      <div className="project-image-container">
         <img src={img} className="project-image" />
       </div>
     </div>
