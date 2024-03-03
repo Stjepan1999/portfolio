41d11aff9990bfbc16e32b1c07487ee298714d02
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-03-03 20:32:55 +0100

    Add resume image and resume download button

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+import resumeImage from '../../assets/Images/resume.jpg';
+import resumePDF from '../../assets/resume.pdf';
 import './ResumePage.scss';
 
 export const ResumePage = () => {
-  return <div className="page">Resume Page</div>;
+  return (
+    <div className="page resume-page">
+      <h1 className="text-primary-color">Resume</h1>
+      <div className="image-container">
+        <img src={resumeImage} type="application/pdf" className="resume-image" />
+      </div>
+      <a
+        href={resumePDF}
+        download="Stjepan_Franić_Resume"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button button-primary"
+      >
+        Download
+      </a>
+    </div>
+  );
 };
